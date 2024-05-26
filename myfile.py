9f0afe643cc84a364a592d744158219dff0759e7
DenysMkl <den.mikliaiev@gmail.com>
2024-05-26 17:23:26 +0300

    create fight

--- myfile.py
+++ myfile.py
@@ -13,6 +13,7 @@ class Bot:
         self.received_shoots = set()
         self.boats = self.create_boats()
         self.previous_shoot = None
+        self.all_shots = []
         self.fill_area()
 
     def fill_area(self):
@@ -26,10 +27,17 @@ class Bot:
                 SingleDeck(), SingleDeck()]
 
     def make_shoot(self, player):
-        letter = choice(alp[:10])
-        number = choice([str(i) for i in range(1, 11)])
-        self.previous_shoot = letter + number
+        self.previous_shoot = self.generate_new_shot()
         player.process_shot_result(self.previous_shoot)
+
+    def generate_new_shot(self):
+        while True:
+            letter = choice(alp[:10])
+            number = choice([str(i) for i in range(1, 11)])
+            new_shot = f'{letter}{number}'
+            if new_shot not in self.all_shots:
+                self.all_shots.append(new_shot)
+                return new_shot
 
     def generate_next_shoots(self, player):
         sides = [(1, 0), (-1, 0), (0, -1), (0, 1)]
@@ -37,12 +45,22 @@ class Bot:
         serialized_x, serialized_y = self.deserialize_shoot(self.previous_shoot)
         filter_sides = [(x, y) for x, y in sides if check_existence(serialized_x + x, serialized_y + y)]
 
+        if not player.check_shot(self.previous_shoot):
+            return
         for x_side, y_side in filter_sides:
-            next_letter = alp[self.get_letter_dict().get(curr_letter) + y_side]
-            next_numb = int(curr_numb) + x_side
-            print(f'{curr_letter}{curr_numb} -> {next_letter}{next_numb}')
-
-    def next_shoot(self, direction): pass
+            counter = 1
+            while True:
+                next_letter = alp[self.get_letter_dict().get(curr_letter) + y_side * counter]
+                next_numb = int(curr_numb) + x_side * counter
+                next_coord = f'{next_letter}{next_numb}'
+                if not check_existence(self.get_letter_dict().get(next_letter, 11),
+                                       int(next_numb)-1):
+                    break
+                player.process_shot_result(next_coord)
+                if not player.check_shot(next_coord):
+                    break
+                self.all_shots.append(next_coord)
+                counter += 1
 
     def valid_shoot(self, x_coord, y_coord):
         return (x_coord, y_coord) in self.received_shoots or \
@@ -65,10 +83,12 @@ class Bot:
             return
         return None if x is None or y is None else (x, y)
 
-    def check_shot(self, x, y):
+    def check_shot(self, coords):
+        x, y = self.deserialize_shoot(coords)
         return self.area[x][y] == '1'
 
     def process_shot_result(self, shot):
+
         if self.deserialize_shoot(shot) is None:
             return
         enemy_x, enemy_y = self.deserialize_shoot(shot)
@@ -77,33 +97,22 @@ class Bot:
             return
         self.received_shoots.add((enemy_x, enemy_y))
         self.hidden_area[enemy_x][enemy_y] = '+' if \
-            self.check_shot(enemy_x, enemy_y) else 'x'
-
+            self.check_shot(shot) else 'x'
         for boat in self.boats:
             if all(map(lambda x: x in self.received_shoots, boat.boat_coords)):
                 boat.mark_fields(self.hidden_area)
                 self.boats.remove(boat)
-        self.show_maps()
 
     def show_maps(self):
         for i, j in zip(self.area, self.hidden_area):
             print(i, j, sep='\t\t')
+        print(end='\n\n\n\n')
 
 
-# b1 = Bot()
-# b2 = Bot()
-# b2.show_maps()
-# b1.make_shoot(b2)
-# b1.generate_next_shoots(b2)
+b1 = Bot()
+b2 = Bot()
 
-# b2.show_maps()
-# while len(b1.boats):
-    # shoot = input('enter square: ').strip()
-    # b1.process_shot_result(shoot)
-
-    # time.sleep(5)
-def factor(n):
-    return n * factor(n-1) if n >= 1 else 1
-
-
-print(factor(5))
+while len(b2.boats):
+    b1.make_shoot(b2)
+    b1.generate_next_shoots(b2)
+    b2.show_maps()
